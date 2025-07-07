7187483413503e9ccf0b941be863b5b3d1a0d32a
tanujgupta2802 <tanujg2802@gmail.com>
2025-07-07 18:27:45 +0530

    Adding project

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path");
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+const users = {};
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  console.log("New client connected:", socket.id);
+
+  socket.on("register-user", (username) => {
+    users[socket.id] = username;
+    io.emit("update-user-count", Object.keys(users).length);
+  });
+
+  socket.on("send-location", (data) => {
+    const username = users[socket.id] || "Anonymous";
+    io.emit("recieve-location", {
+      id: socket.id,
+      username,
+      ...data,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+    delete users[socket.id];
+    io.emit("user-disconnected", socket.id);
+    io.emit("update-user-count", Object.keys(users).length);
+  });
+});
+
+app.get("/api/hello", (req, res) => {
+  res.render("index");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
---
+++ public/js/script.js
@@ -0,0 +1,70 @@
+const socket = io({ forceNew: true });
+
+console.log("script.js is loaded");
+
+let username = prompt("Enter your name:") || "Anonymous";
+socket.emit("register-user", username);
+
+socket.on("connect", () => {
+  console.log("Connected with socket id:", socket.id);
+});
+
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      console.log("Sending location:", latitude, longitude);
+      socket.emit("send-location", { latitude, longitude });
+    },
+    (error) => {
+      console.log("Location error:", error);
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    }
+  );
+} else {
+  console.log("Geolocation not supported");
+}
+
+const map = L.map("map").setView([0, 0], 16);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "OpenStreetMap contributors",
+}).addTo(map);
+
+const markers = {};
+
+socket.on("recieve-location", (data) => {
+  const { id, latitude, longitude, username } = data;
+
+  console.log("Location received from:", id, latitude, longitude);
+
+  if (id === socket.id) {
+    map.setView([latitude, longitude]);
+  }
+
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude])
+      .addTo(map)
+      .bindPopup(`${username}`)
+      .openPopup();
+  }
+});
+
+socket.on("user-disconnected", (id) => {
+  console.log("Removing marker of:", id);
+  if (markers[id]) {
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
+});
+
+socket.on("update-user-count", (count) => {
+  const counter = document.getElementById("user-count");
+  if (counter) counter.innerText = `Online Users: ${count}`;
+});
