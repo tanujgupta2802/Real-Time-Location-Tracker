014db13978ad555dd992db9285a6de27b2e3cf99
tanujgupta2802 <tanujg2802@gmail.com>
2025-07-07 19:21:12 +0530

    adding PORT

--- app.js
+++ app.js
@@ -42,6 +42,12 @@ app.get("/api/hello", (req, res) => {
   res.render("index");
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+// server.listen(3000, () => {
+//   console.log("Server running on port 3000");
+// });
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
